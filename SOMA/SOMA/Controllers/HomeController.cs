64a5ade596bc0a72a729f8bc6487ee4ddc3cd3ca
Henrique Valvassori <henriquevlvassori41@gmail.com>
2024-09-13 19:54:24 -0300

    exercicio numero 1 do desafio TARGET SISTEMAS

---
+++ SOMA/SOMA/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SOMA.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            int SOMA = 0;
+            int INDICE = 13;
+            int k = 0;
+            while (k < INDICE)
+            {
+
+
+                k = k + 1;
+                SOMA = SOMA + k;
+
+
+
+            }
+            ViewBag.Message = SOMA;
+            return View();
+        }
+
+       
+    }
+}
